62ac5e0fc596f6dcf5de10dc97b9320d43f4c647
shokasuya <shou2379@gmail.com>
2020-10-30 23:15:14 +0900

    javascript

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do
-  factory :message do
-    content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
-    user
-    group
-  end
-end
+# FactoryBot.define do
+#   factory :message do
+#     content {Faker::Lorem.sentence}
+#     image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+#     user
+#     group
+#   end
+# end
